566e7c3b6e91ba6684031c1d3340fd89ae4c0b7d
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-09-28 18:21:46 +0300

    DONE

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const getActrs= actors.map((act)=>(
+    <div key={act.name} >
+      <h2>{act.name}</h2>
+      <ul>
+        {act.movies.map((movie)=>(
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+
+  )) 
+
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {getActrs}
+    </div>
+   
+  )
 }
 
 export default Actors;
